ea58d61ce20a3cc87a7848fdaea5e3784f7fe1d7
Jennifer Hung <jennifersukay505@gmail.com>
2015-11-13 21:59:25 -0700

    ghost writing

--- index.php
+++ index.php
@@ -1,9 +1,362 @@
 <!DOCTYPE html>
-	<html lang="en">
+<html lang="en">
 	<head>
-	<title>ghost write</title>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!--font styles-->
+		<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Acme">
+
+		<!--javascript-->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
+				integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
+				crossorigin="anonymous">
+		<!--font-awesome-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"
+				integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+
+		<!--Animate.css-->
+		<link rel="stylesheet" href="://cdnjs.cloudflare.com/ajax/libs/animate.css/3.4.0/animate.min.css">
+
+		<!--Link to custom CSS files here-->
+		<link type="text/css" href="/css/sidepanel.css" rel="stylesheet">
+		<link type="text/css" href="/css/footer.css" rel="stylesheet">
+		<link type="text/css" href="/css/style.css" rel="stylesheet">
+
+
+		<!--HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries-->
+		<!--WARNING: Respond.js doesn't work if you view the page via file://-->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!--js-cookie (for capstone) -->
+		<script type="text/javascript" src="//cdnjs.cloudfare.com/ajax/libs/js-cookie/2.0.2/js.cookie.min.js"></script>
+
+		<!--jQuery (necessary for bootstrap's javaScript plugins)-->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+		<!-- jQuery form validation and additional methods (for capstone) -->
+		<script type="text/javascript" src="//cdnjs.cloudfare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudfare/com/ajax/libs/jquery-validate/1.14.0/jquery.valiate.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudfare.com/ajax/libs/jquery-validate/1.14.0.additional-methods.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"
+				  integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ=="
+				  crossorigin="anonymous"></script>
+
+		<!-- jscroll plugin -->
+		<script src="../../lib/plugins/jscroll/jquery.jscroll.min.js" type="text/javascript"></script>
+
+		<!-- Page Title -->
+		<title>Homepage Outline</title>
 	</head>
-	<body>
-		<h1>ghost write index</h1></ht>
+
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<!--include the <head> tag-->
+			<header>
+				<nav class="navbar navbar-default navbar-fixed-top">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar" href="#">Brand</a>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+
+						<form class="navbar-form navbar-left" role="search">
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Search">
+							</div>
+							<button type="submit" class="btn btn-default">Submit</button>
+						</form>
+						<ul class="nav navbar-nav navbar-right">
+
+							<li><a href="#"><i class="fa fa-home fa-fw"></i>&nbsp; Home</a></li>
+							<li><a href="#"><i class="fa fa-user"></i>&nbsp;Settings</a></li>
+							<li><a href="#"><i class="fa fa-envelope"></i>&nbsp;Messages</a></li>
+							<li><a href="#"><i class="fa fa-question"></i>&nbsp;Help</a></li>
+
+						</ul>
+					</div>
+				</nav>
+			</header>
+			<!-- side panel here -->
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 hidden-xs hidden-sm" id="sidepanel">
+						<section class="side-panel panel panel-default">
+							<ul>
+								<li><br></li>
+								<li><br></li>
+								<li><br></li>
+								<li><br></li>
+
+								<li><a href="#"><i class="fa fa-pencil"></i>&nbsp;Settings</a></li>
+								<li><br></li>
+
+								<li><a href="#"><i class="fa fa-envelope"></i>&nbsp;Messages</a></li>
+								<li><br></li>
+
+
+
+								<li><br></li>
+								<li><br></li>
+								<li><br></li>
+								<li><br></li>
+								<li><br></li>
+								<li><br></li>
+								<li><br></li>
+								<li><br></li>
+								<li><br></li>
+								<li><br></li>
+
+							</ul>
+						</section>
+					</div>
+
+					<!-- main content area -->
+					<div class="clearfix"</div>
+				<div class="col-md-8 col-xs-12">
+					<main>
+						<p><br></p>
+						<p><br></p>
+						<!-- load feed -->
+						<div class="feed-wrapper">
+
+							<div id="karma-feed" class="karma-feed">
+
+								<h1>Suggested Tweets</h1>
+
+								<div class="listing clearfix panel panel-default">
+									<div class="panel-heading">
+										<h4 class="listing-title">
+											Tweet title here
+										</h4>
+									</div>
+								</div>
+
+								<div class="panel-body">
+									<p class="listing-text">
+										Tweet text here
+									</p>
+									<div class="panel-offer">
+										<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+											Push to twitter
+										</button>
+										<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+											Edit
+										</button>
+										<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+											Decline
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+
+
+						<div class="listing clearfix panel panel-default">
+							<div class="panel-heading">
+								<h4 class="listing-title">
+									Tweet title here
+								</h4>
+							</div>
+							<div class="panel-body">
+								<p class="listing-text">
+									Tweet text here.
+								</p>
+								<div class="panel-offer">
+									<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+										Push to twitter
+									</button>
+									<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+										Edit
+									</button>
+									<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+										Decline
+									</button>
+								</div>
+							</div>
+						</div>
+
+						<div class="listing clearfix panel panel-default">
+							<div class="panel-heading">
+								<h4 class="listing-title">
+									Twitter title here.
+								</h4>
+							</div>
+							<div class="panel-body">
+								<img src="http://placehold.it/60x60" alt="thumbnail image" class="img-thumbnail pull-left">
+								<p class="listing-text">
+									Twitter text here.
+								</p>
+								<div class="panel-offer">
+									<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+										Push to twitter
+									</button>
+									<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+										Edit
+									</button>
+									<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+										Decline
+									</button>
+
+								</div>
+							</div>
+						</div>
+
+						<div class="listing clearfix panel panel-default">
+							<div class="panel-heading">
+								<h4 class="listing-title">
+									Twitter title here.
+								</h4>
+							</div>
+							<div class="panel-body">
+								<p class="listing-text">
+									Twitter text here.
+								</p>
+								<div class="panel-offer">
+									<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+										Push to twitter
+									</button>
+									<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+										Edit
+									</button>
+									<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+										Decline
+									</button>
+
+							</div>
+						</div>
+
+						<div class="listing clearfix panel panel-default">
+							<div class="panel-heading">
+								<h4 class="listing-title">
+									Twitter title here.
+								</h4>
+							</div>
+							<div class="panel-body">
+								<p class="listing-text">
+									Twitter text here.
+								</p>
+								<div class="panel-offer">
+									<div class="panel-offer">
+										<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+											Push to twitter
+										</button>
+										<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+											Edit
+										</button>
+										<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+											Decline
+										</button>
+								</div>
+							</div>
+						</div>
+
+							<div class="listing clearfix panel panel-default">
+								<div class="panel-heading">
+									<h4 class="listing-title">
+										Twitter title here.
+									</h4>
+								</div>
+								<div class="panel-body">
+									<p class="listing-text">
+										Twitter text here.
+									</p>
+									<div class="panel-offer">
+										<div class="panel-offer">
+											<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+												Push to twitter
+											</button>
+											<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+												Edit
+											</button>
+											<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+												Decline
+											</button>
+										</div>
+									</div>
+								</div>
+
+								<div class="listing clearfix panel panel-default">
+									<div class="panel-heading">
+										<h4 class="listing-title">
+											Twitter title here.
+										</h4>
+									</div>
+									<div class="panel-body">
+										<p class="listing-text">
+											Twitter text here.
+										</p>
+										<div class="panel-offer">
+											<div class="panel-offer">
+												<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+													Push to twitter
+												</button>
+												<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+													Edit
+												</button>
+												<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+													Decline
+												</button>
+											</div>
+										</div>
+									</div>
+
+									<div class="listing clearfix panel panel-default">
+										<div class="panel-heading">
+											<h4 class="listing-title">
+												Twitter title here.
+											</h4>
+										</div>
+										<div class="panel-body">
+											<p class="listing-text">
+												Twitter text here.
+											</p>
+											<div class="panel-offer">
+												<div class="panel-offer">
+													<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+														Push to twitter
+													</button>
+													<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+														Edit
+													</button>
+													<button class="listing-button" type="button" class="btn btn-primary btn-md" data-toggle="modal" data-target="#myModal">
+														Decline
+													</button>
+												</div>
+											</div>
+										</div>
+
+
+
+						<!--.sfooter-content-->
+
+						<footer class="footer navbar-fixed-bottom">
+							<div class="container">
+								<a href="#">About </a>&nbsp;
+								<a href="#">FAQ </a>&nbsp;
+								<a href="#">Contact Us</a>
+							</div>
+						</footer>
 	</body>
+	<script type="text/javascript" src="../js/form-scripts.js"></script>
 </html>
